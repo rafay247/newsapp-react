643398a4ea5b786cde302ecba2a715d4e00766c3
rafay <rafayf3@gmail.com>
2022-10-10 20:33:12 +0545

    looping

--- src/components/News.js
+++ src/components/News.js
@@ -67,20 +67,13 @@ export default class News extends Component {
     return (
       <div className="container my-3">
         <h1 className='my-4'>News App - top headlines</h1>
-
-        <div className="row mb-5">
-
-          <div className="col-md-4">
-            <NewsItem title="mytiltle" discription="mydes" imageUrl="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/PTC3FNFA2QI35YZG3UEQB2J4AE.JPG&w=1440" />
+        {this.state.articles.map(() => {
+          return <div className="row mb-5">
+            <div className="col-md-4">
+              <NewsItem title="mytiltle" discription="mydes" imageUrl="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/PTC3FNFA2QI35YZG3UEQB2J4AE.JPG&w=1440" />
+            </div>
           </div>
-          <div className="col-md-4">
-            <NewsItem title="mytiltle" discription="mydes" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="mytiltle" discription="mydes" />
-          </div>
-
-        </div>
+        })}
 
       </div>
     )
