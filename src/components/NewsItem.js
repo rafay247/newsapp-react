1c70f8e1ff4f6ce283d4cd6ed9f31eca490478a0
rafay <rafayf3@gmail.com>
2022-10-10 14:50:35 +0545

    adding features

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
+    let {title, discription} = this.props;
     return (
-      <div>
-        This is news item 2
+      <div className="card" style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{discription}</p>
+          <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     )
   }
