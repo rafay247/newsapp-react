473e58dcf4a4aa0dc7b6d9a398ce3f5186fc0abf
rafay <rafayf3@gmail.com>
2022-10-10 15:02:39 +0545

    making news items

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,24 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is news commponent
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container my-3">
+        <h1 className='my-4'>News App - top headlines</h1>
+
+        <div className="row">
+
+          <div className="col-md-4">
+            <NewsItem title="mytiltle" description="mydes" />
+          </div>
+
+          <div className="col-md-4">
+            <NewsItem title="mytiltle" description="mydes" />
+          </div>
+
+          <div className="col-md-4">
+            <NewsItem title="mytiltle" description="mydes" />
+          </div>
+          
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,8 +4,8 @@ export default class NewsItem extends Component {
   render() {
     let {title, discription} = this.props;
     return (
-      <div className="card" style="width: 18rem;">
-        <img src="..." className="card-img-top" alt="..." />
+      <div className="card" style={{width: "18rem"}}>
+        <img src="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/PTC3FNFA2QI35YZG3UEQB2J4AE.JPG&w=1440" className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{discription}</p>
