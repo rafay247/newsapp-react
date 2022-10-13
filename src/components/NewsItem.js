1775eebd9e5e3e2b06de255da53a467e2cdf18de
rafay <rafayf3@gmail.com>
2022-10-13 20:21:07 +0545

    fixing issue

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export default class NewsItem extends Component {
         <div className="card" style={{ width: "18rem" }}>
           <img src={!imageUrl ? "https://static.foxnews.com/foxnews.com/content/uploads/2020/04/blue-angles-1-US-NAVY.jpg" : imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h4 className="card-title">{title}</h4>
             <p className="card-text">{discription}</p>
             <a href={newsUrl} target = "_blank"  rel="noreferrer" className="btn btn-sm btn-dark">Read more</a>
           </div>
