6b174df0497104b489fb3a67589643a404b234b1
rafay <rafayf3@gmail.com>
2022-10-15 12:51:36 +0545

    routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
-        <News pageSize = {5} country = "us" category = "science" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" />
+        <Routes>
+          <Route exact path= "/"><News pageSize = {5} country = "us" category = "general" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" /> </Route>
+          <Route exact path= "/business"><News pageSize = {5} country = "us" category = "business" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" /> </Route>
+          <Route exact path= "/entertainment"><News pageSize = {5} country = "us" category = "entertainment" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" /> </Route>
+          <Route exact path= "/general"><News pageSize = {5} country = "us" category = "general" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" /> </Route>
+          <Route exact path= "/health"><News pageSize = {5} country = "us" category = "health" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" /> </Route>
+          <Route exact path= "/science"><News pageSize = {5} country = "us" category = "science" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" /> </Route>
+          <Route exact path= "/sports"><News pageSize = {5} country = "us" category = "sports" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" /> </Route>
+          <Route exact path= "/technology"><News pageSize = {5} country = "us" category = "technology" apiKey = "dbfdd9ebf09a476bb8d34b8ca66ae9c2" /> </Route>
+        </Routes>
+          
+        </Router>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ export default class NewsItem extends Component {
     let { title, discription, imageUrl, newsUrl } = this.props;
     return (
       <div className="container my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img src={!imageUrl ? "https://static.foxnews.com/foxnews.com/content/uploads/2020/04/blue-angles-1-US-NAVY.jpg" : imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h4 className="card-title">{title}</h4>
